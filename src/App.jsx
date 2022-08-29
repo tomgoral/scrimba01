1a89b3c43e0af2345282a94041d84d15996a0ab1
Tom Goral <tg@tomgoral.com>
2022-08-29 19:02:13 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+import Navbar from "./components/Navbar.jsx";
+import Content from "./components/Content.jsx";
+
+
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <Content />
+    </div>
+  )
+}
+
